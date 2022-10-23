9517b5de597ebd84ef6d1c237f86a1222f7492f2
Aya <90342033+Aya-X@users.noreply.github.com>
2022-10-24 01:20:33 +0800

    feat: Update jwt-decode to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 
+const jwt_decode = require('jwt-decode');
+const JWT_SECRET_KEY =
+  require('./node_modules/json-server-auth/dist/constants').JWT_SECRET_KEY;
+
 const port = process.env.PORT || 3000;
 
 const server = jsonServer.create();
@@ -23,6 +27,31 @@ server.db = router.db;
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    const token = req.header('Authorization')
+      ? req.header('Authorization').replace('Bearer ', '')
+      : null;
+
+    if (token) {
+      const decoded = jwt_decode(token);
+      console.log({ token, JWT_SECRET_KEY, decoded });
+
+      const intSub = Number(decoded.sub);
+      req.body.userId = intSub;
+    }
+    /* end of IF-token */
+  }
+
+  // Continue to JSON Server router
+  next();
+});
+/* end of custom */
+
 server.use(rules);
 
 // You must apply the auth middleware before the router
@@ -30,10 +59,6 @@ server.use(auth);
 
 // Use default router
 server.use('/api', router);
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
 
 server.listen(port, () => {
   console.log('JSON Server Listening on:' + port);
